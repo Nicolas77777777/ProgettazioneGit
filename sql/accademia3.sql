fa4bc3bbf2882520d0ff508518b011edc0c5eadf
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2024-09-22 14:52:43 +0200

    farmacia

--- sql/accademia3.sql
+++ sql/accademia3.sql
@@ -1,3 +1,5 @@
+-- Definizione dei domini
+
 create type Strutturato as 
 enum (Ricercatore, Professore Associato, Professore Ordinario)
 
@@ -14,11 +16,39 @@ create type CausaAssenza as enum
 create domain PosInteger as integer
 CHECK (VALUE >= 0);
 
-• StringaM
-varchar(100)
+create domain StringaM as varchar (100);
 
 create domain NumeroOre as integer
-CHECK  (VALUE >= 0 and VALUE <=8)
+CHECK  (VALUE >= 0 and VALUE <=8);
 
-• Denaro
-real ≥ 0
+CREATE DOMAIN Denaro AS REAL
+CHECK (VALUE >= 0);
+
+--Schema relazionale con vincoli della base dati
+CREATE TABLE Persona (
+    id PosInteger NOT NULL, -- PRIMARY KEY
+    nome StringaM  NOT NULL ,
+    cognome  StringaM   NOT NULL, 
+    posizione Strutturato  NOT NULL,
+    stipendio Denaro   NOT NULL)
+    (id) PRIMARY KEY
+
+Progetto (id: PosInteger, nome: StringaM, inizio: date, fine: date, budget:
+Denaro)
+[VincoloDB.1] altra chiave: (nome)
+[VincoloDB.2] ennupla: inizio < fine
+WP (progetto: PosInteger, id: PosInteger, nome: StringaM, inizio: date, fine:
+date)
+[VincoloDB.3] ennupla: inizio < fine
+[VincoloDB.4] altra chiave: (progetto, nome)
+[VincoloDB.5] foreign key: progetto references Progetto(id)
+AttivitaProgetto (id: PosInteger, persona: PosInteger, progetto: PosInteger,
+wp: PosInteger, giorno: date, tipo: LavoroProgetto, oreDurata: NumeroOre)
+[VincoloDB.6] foreign key: persona references Persona(id)
+[VincoloDB.7] foreign key: (progetto, wp) references WP(progetto, id)
+AttivitaNonProgettuale (id: PosInteger, persona: PosInteger, tipo: Lavoro-
+NonProgettuale, giorno: date, oreDurata: NumeroOre)
+[VincoloDB.8] foreign key: persona references Persona(id)
+Assenza (id: PosInteger, persona: PosInteger, tipo: CausaAssenza, giorno: date)
+[VincoloDB.9] altra chiave: persona, giorno
+[VincoloDB.10] foreign key: persona references Persona(id)
---
+++ sql/farmacia.sql
@@ -0,0 +1,8 @@
+create type Ruolo as 
+enum ('Paziente', 'Dottore')
+
+create type Farmaco as 
+enum ('Antibiotici', '')
+
+
+
