4d5b4fbb8e122683f9035b5bbe682d709558110a
Nicolas7777777 <sairwater@gmail.com>
2024-09-30 13:43:32 +0200

    go ristrutturazione

---
+++ UML/officina/officina.sql
@@ -0,0 +1,50 @@
+CREATE TABLE Nazione (
+    nome varchar(100) NOT NULL,
+    PRIMARY KEY (nome)
+);
+
+CREATE TABLE Regione (
+    nome varchar(100) NOT NULL,
+    nazione varchar (100) NOT NULL,
+    PRIMARY KEY (nome, nazione) NOT NULL, 
+    FOREIGN KEY (nazione) REFERENCES Nazione (nome)
+);
+
+CREATE TABLE Citta (
+    nome varchar(100) NOT NULL,
+    nazione varchar(100) NOT NULL, 
+    regione varchar(100) NOT NULL, 
+    PRIMARY KEY (nome,regione,Nazione),
+    FOREIGN KEY (regione, nazione) REFERENCES Regione(nome,nazione) 
+);
+
+CREATE TABLE Marca (
+    nome varchar (100) NOT NULL,
+    PRIMARY KEY (nome)
+);
+
+CREATE TABLE TipoVeicolo(
+    nome varchar (100) NOT NULL,
+    PRIMARY KEY (nome)
+);
+
+CREATE TABLE Modello (
+    nome varchar (100) NOT NULL,
+    marca varchar(100) NOT NULL,
+    tipo varchar(100) NOT NULL,
+    PRIMARY KEY (nome,marca),
+    FOREIGN KEY (marca) REFERENCES Marca(nome),
+    FOREIGN KEY (tipo) REFERENCES TipoVeicolo(nome)
+);
+
+CREATE TABLE Veicolo (
+    targa  varchar PRIMARY KEY NOT NULL,
+    immatricolazione INTEGER NOT NULL,
+    modello varchar (100) NOT NULL,
+    FOREIGN KEY (modello) REFERENCES modello (nome)
+);
+
+CREATE TABLE Cliente(
+
+);
+
